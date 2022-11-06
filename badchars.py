b3b47b94e02b1de2dd6f80a13e697a9939367603
n0bugz <68533113+n0bugz@users.noreply.github.com>
2022-11-06 08:41:21 -0500

    Initial commit

---
+++ badchars.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+listRem = "".split("\\x")
+for x in range(1, 256):
+    if "{:02x}".format(x) not in listRem:
+        print("\\x" + "{:02x}".format(x), end='')
+print()
